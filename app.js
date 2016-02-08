779168340a6ca0d7637b5518eb00f7df38bf96de
Trinity Xia <hnaoto@me.com>
2016-02-08 18:58:28 -0500

    fixing cors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var heroes = require('./routes/hero');
 var app = express();
-
+var cors = require('cors'); 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 
 
 

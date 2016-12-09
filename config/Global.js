94892596458b7af375916bde10eb08cd6755d9dd
Trinity Xia <hnaoto@me.com>
2016-12-09 00:48:26 -0500

    create global constant

---
+++ config/Global.js
@@ -0,0 +1,18 @@
+//global constant
+module.exports = {
+    URL: {
+        HF: {
+            home: 'http://www.heroesfire.com',
+            guides: 'http://www.heroesfire.com/hots/guides',
+            guides_l:'http://www.heroesfire.com/hots/guides?fHeroes=',
+            guides_r:'&fType=Guide&fLanguage=&fCategory=&s=t',
+
+
+        },
+
+    },
+
+
+
+
+};

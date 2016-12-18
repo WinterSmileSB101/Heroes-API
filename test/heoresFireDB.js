e81581f64f4c460634bf5c822f4ae961b3b8683b
hnaoto <hnaoto@me.com>
2016-12-18 16:28:44 -0500

    add more test cases

--- test/heoresFireDB.js
+++ test/heoresFireDB.js
@@ -1,10 +1,42 @@
 const assert = require('assert');
-var HeoresFireDB = require('../lib/dataAccess/fireDataAccess.js');
+var mongoClient = require('mongodb').MongoClient;
+var HeroesFireDB = require('../lib/dataAccess/fireDataAccess.js');
 
-var heroesFireDB = new HeoresFireDB();
+
+
+
+var heroesFireDB = new HeroesFireDB();
+var environment = process.env.NODE_ENV || 'development';
+
+
 
 describe('create instance of HeroesFireDB', function () {
-    it('should initialize mongo db collection name', function () {
-        assert.equal('heroes', heroesFireDB.colName);
+		it('should initialize mongo db collection name', function () {
+				assert.equal('heroes', heroesFireDB.colName);
     });
+		
+
+		it('should initialize mongo db connection url', function() {
+				assert.equal( environment == 'development' ? 'mongodb://localhost:27017/heroes' : 'mongodb://chat:nodesimplechat@ds029615.mongolab.com:29615/nodesimplechat', heroesFireDB.dbConnectionUrl);
+		});
+		
+		
+		it('should initilized mongo db client', function() {
+				assert.equal(mongoClient, heroesFireDB.mongoClient);
+		});
+		
 });
+
+
+
+describe('functions of HeroesFireDB', function () {
+		it('should update document', function () {
+				assert.equal('heroes', heroesFireDB.colName);
+    });
+		
+
+		
+});
+
+
+

e344c776bf0b1f75419fecb8be9ad10215aab2ef
hnaoto <hnaoto@me.com>
2016-12-22 03:13:05 -0500

    clean routes

--- routes/hero.js
+++ routes/hero.js
@@ -3,13 +3,7 @@ var router = express.Router();
 var hero = require('../lib/engine/hero');
 var heroesFire = require('../lib/engine/heroesfire');
 
-
-router.get('/get', hero.getByName);
-router.get('/getall', hero.getAll);
 router.get('/ids', heroesFire.heroId);
-//router.get('/guides', heroesFire.guides);
-//router.get('/guides', heroesFire.getGuidesByName);
-//router.get('/sing-guide', heroesFire.singleGuide);
 router.get('/id/update', heroesFire.updateHeroIdCol);
 router.get('/guide/update', heroesFire.updateHeroGuidesById);
 router.get('/guide/update/all', heroesFire.updateAllHeroGuides);

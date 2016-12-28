0d1f4f5f550298fb284bff151edbca6ac0373b90
hnaoto <hnaoto@me.com>
2016-12-28 00:58:31 -0500

    update routes

--- routes/hero.js
+++ routes/hero.js
@@ -4,9 +4,9 @@ var hero = require('../lib/engine/hero');
 var heroesFire = require('../lib/engine/heroesfire');
 
 router.get('/ids', heroesFire.heroId);
-router.post('/id/update', heroesFire.updateHeroId);
-router.post('/guide/update/:id', heroesFire.updateHeroGuidesById);
-router.post('/guide/update/all', heroesFire.updateAllHeroGuides);
+router.put('/id/update', heroesFire.updateHeroId);
+router.put('/guide/update/:id', heroesFire.updateHeroGuidesById);
+router.put('/guide/update/all', heroesFire.updateAllHeroGuides);
 
 module.exports = router;
 

4c2febba78f063a6711d230bdcc175ce2522fafd
hnaoto <hnaoto@me.com>
2017-01-06 02:42:14 -0500

    add more request test cases

--- test/heroesEngineTest.js
+++ test/heroesEngineTest.js
@@ -4,18 +4,22 @@ var app = require('../app');
 var supertest = require('supertest');
 var request = supertest(app);
 
+var heroFixture = require('../test/fixtures/heroes.json');
+var lili = heroFixture.find(x=>x.name='li-li');
+var guideUrl = lili.guides[0].url;
 
-
-describe('herores fire engine test', function(){
+describe('herores fire api test', function(){
 	it('should return guides by hero id', function(done){
 		request.get('/api/guide/all/li-li')
-		.end(function (err, res) {
-			assert.equal(res.statusCode, 200);
-			done(err);
-		});
+		.expect('Content-Type', /json/)
+		.expect(200, lili, done);
+	});
+	
+	it('should return sepecific guide by url', function(done){
+		request.get('/api/guide/get?url=' + guideUrl)
+		.expect('Content-Type', /json/)
+		.expect(200, done);
 	});
 });
-
-
 
 

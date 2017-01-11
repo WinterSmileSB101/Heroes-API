fc360da9f9eb760749133b428f869ce53cacec5f
hnaoto <hnaoto@me.com>
2017-01-10 23:23:44 -0500

    add sort function and test cases

--- test/heroesEngineTest.js
+++ test/heroesEngineTest.js
@@ -5,6 +5,7 @@ var supertest = require('supertest');
 var request = supertest(app);
 
 var heroFixture = require('../test/fixtures/heroes.json');
+var sortedHeroFixture = require('../test/fixtures/sortedHeroes.json');
 var lili = heroFixture.find(x=>x.name='li-li');
 var guideUrl = lili.guides[0].url;
 
@@ -13,6 +14,19 @@ describe('herores fire api test', function(){
 		request.get('/api/guide/all/li-li')
 		.expect('Content-Type', /json/)
 		.expect(200, lili, done);
+	});
+	
+	
+	it('should return guides by hero id with sort by votes', function(done){
+		request.get('/api/guide/all/li-li?sort=votes')
+		.expect('Content-Type', /json/)
+		.expect(200, sortedHeroFixture['votes'], done);
+	});
+	
+	it('should return guides by hero id with sort by date', function(done){
+		request.get('/api/guide/all/li-li?sort=date')
+		.expect('Content-Type', /json/)
+		.expect(200, sortedHeroFixture['date'], done);
 	});
 	
 	it('should return sepecific guide by url', function(done){

257fe8bbab91d83272d618ddd69194cb7c88c22b
hnaoto <hnaoto@me.com>
2017-01-18 23:36:18 -0500

    update view

--- public/javascripts/hexgon.js
+++ public/javascripts/hexgon.js
@@ -6,18 +6,17 @@
 	var hexagon = $('<div class="boxF"> <div class="boxS"> <div class="boxT">   <div class="overlay"></div></div> </div> </div>');
 	
 	var data = heroes;
-	var n = (data.length / 13) * 2 ;	
+	var n = (data.length / 13) * 2 ;
 		for (var i = 0; i < 52;  i++) {
 				var boxF = $('<div class="boxF">  </div>');
 				var boxS = $('<div class="boxS">   </div>');
-				var boxT = $('<div class="boxT"> </div>');
-
+				var boxT = $('<a class="boxT"> </a>');
+				var guideUrl = '/api/guide/all/' + data[i]['name'];
+				boxT.attr('href', guideUrl);
 				var icon_url = 'http://www.heroesfire.com/images/wikibase/icon/heroes/' + data[i]['name'] + '.png';
 				boxT.attr('style', 'background-image:url(' +  icon_url +  ')' );
-				
-				var overlay =  data[i]['Free'] ?   $('<div class="overlay free" >   </div> ') : $('<div class="overlay" >  </div> ');
-				
-				
+				var overlay = $(data[i]['Free'] ?   $('<div class="overlay free" >   </div> ') : $('<div class="overlay" >  </div> '));
+ 
 				//var info_position = 'top:' + 58.24 * i +  'px; ' +   'left:' + 33.6 * (i % 2) +  'px;'    ;
 				//var li = $('<li> </li>');
 				

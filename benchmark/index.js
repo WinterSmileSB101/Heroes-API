cb7144117643f9d0b0c8bbe07606a6f3cfd33500
hnaoto <hnaoto@me.com>
2017-01-21 23:37:10 -0500

    update benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -2,11 +2,14 @@ var assert = require('assert');
 var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
 var HeroesFireDB = require('../lib/dataAccess/HeroesFireDB');
-var heroesFireDB = new HeroesFireDB();
 var heroesFire = require('../lib/engine/heroesFire');
+var util = require('util');
+
+var heroesFireDB = new HeroesFireDB();
+
 
 suite
-	.add('HeroesFireDB get guides by name',{
+	.add('HeroesFireDB: get guides by name',{
 		defer: true,
 		fn: function (deferred) {
 				heroesFireDB.get({name: 'li-li'}, function(){
@@ -14,13 +17,25 @@ suite
 				});
 		}
 	})
-	.add('HeroesFireDB get sorted guides by name',{
+	.add('HeroesFireDB: get sorted guides by votes',{
 		defer: true,
 		fn: function (deferred) {
 				heroesFireDB.getSorted({name: 'li-li'}, 'votes', function(){
 					deferred.resolve();
 				});
 		}
+	})
+	.add('HeroesFireDB: get sorted guides by date',{
+		defer: true,
+		fn: function (deferred) {
+				heroesFireDB.getSorted({name: 'li-li'}, 'date', function(){
+					deferred.resolve();
+				});
+		}
+	})
+	.on('cycle', function(event) {
+		console.log(String(event.target));
+		console.log(util.inspect(process.memoryUsage()));
 	})
 	.on('complete', function(){
 		console.log('results: ');

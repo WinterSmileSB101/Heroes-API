23e4e418815c399ac274a589bd92e7b1295a9760
hnaoto <hnaoto@me.com>
2017-02-03 00:31:35 -0500

    add update all guides function

--- routes/hero.js
+++ routes/hero.js
@@ -5,7 +5,7 @@ var heroesFire = require('../lib/engine/heroesfire');
 
 router.put('/id/update', heroesFire.updateHeroId);
 router.put('/guide/update/:id', heroesFire.updateHeroGuidesById);
-router.put('/guide/update/all', heroesFire.updateAllHeroGuides);
+router.put('/guides/update/', heroesFire.updateAllHeroGuides);
 
 module.exports = router;
 
